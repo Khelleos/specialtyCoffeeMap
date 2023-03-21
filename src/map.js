b0b40240d8262ac8bc388bbce41c29a51e6abcbb
Alexey Khaneft <a.khaneft@gmail.com>
2023-03-21 23:45:18 +0100

    refactor css and add instagram logo

--- src/map.js
+++ src/map.js
@@ -7,16 +7,17 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
 export function createMarkers() {
   window.bookmarks.forEach((bookmark) => {
     const now = new Date();
-    const dayOfWeek = now.getUTCDay(); // 0 = Sunday, 1 = Monday, etc.
+    const dayOfWeek = now.getUTCDay();
     const hourOfDay = now.getUTCHours();
     const isOpen = bookmark.workingHours[dayOfWeek] && hourOfDay >= bookmark.workingHours[dayOfWeek][0] && hourOfDay < bookmark.workingHours[dayOfWeek][1];
-    const statusColor = isOpen ? "green" : "red";
+    const statusClass = isOpen ? "open" : "closed";
 
     const popupContent = `
     <div class="popup-title">
+      <img src="assets/instagram.png" alt="Instagram" width="20" height="20">
       <a href="${bookmark.instagramLink}" target="_blank" class="popup-title-text" style="font-size: 20px;">${bookmark.name}</a>
     </div>
-    <div class="popup-status" style="margin-top: 10px; margin-bottom: 10px; color: ${statusColor};">${isOpen ? "Open" : "Closed"}</div>
+    <div class="popup-status ${statusClass}">${isOpen ? "Open" : "Closed"}</div>
     <div class="popup-address">${bookmark.address}</div>
       `;
 
