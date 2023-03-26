c3e5fe50cc003e9cefb0255eb173293eb5583b60
Alexey Khaneft <a.khaneft@gmail.com>
2023-03-26 03:05:20 +0200

    adjusted model of bookmarks and calculate status onClick action

--- src/map.js
+++ src/map.js
@@ -9,29 +9,44 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
   attribution: "&copy; CartoDB",
 }).addTo(map);
 
-/**
- * Create markers on the map
- */
+function isCafeOpen(bookmark) {
+  const now = new Date();
+  const dayOfWeek = now.getUTCDay();
+  const hourOfDay = now.getUTCHours();
+  const minuteOfDay = now.getUTCMinutes();
+  const currentWorkingHours = bookmark.workingHours[dayOfWeek];
+
+  if (!currentWorkingHours) {
+    return false;
+  }
+
+  const [openHour, openMinute] = currentWorkingHours.openAt.split(':');
+  const [closeHour, closeMinute] = currentWorkingHours.closeAt.split(':');
+
+  const openTime = Number(openHour) * 60 + Number(openMinute);
+  const closeTime = Number(closeHour) * 60 + Number(closeMinute);
+  const currentTime = hourOfDay * 60 + minuteOfDay;
+
+  return currentTime >= openTime && currentTime < closeTime;
+}
+
 export function createMarkers() {
   window.bookmarks.forEach((bookmark) => {
-    const now = new Date();
-    const dayOfWeek = now.getUTCDay();
-    const hourOfDay = now.getUTCHours();
-    const isOpen = bookmark.workingHours[dayOfWeek] && hourOfDay >= bookmark.workingHours[dayOfWeek][0] && hourOfDay < bookmark.workingHours[dayOfWeek][1];
-    const statusClass = isOpen ? 'open' : 'closed';
-
-    const popupContent = `
-            <div class="popup-title">
-                <img src="${instagramLogo}" alt="Instagram" width="20" height="20">
-                <a href="${bookmark.instagramLink}" target="_blank" class="popup-title-text" style="font-size: 20px;">${bookmark.name}</a>
-            </div>
-            <div class="popup-status ${statusClass}">${isOpen ? 'Open' : 'Closed'}</div>
-            <div class="popup-address">${bookmark.address}</div>
-        `;
-
     const marker = L.marker([bookmark.lat, bookmark.lng]).addTo(map);
 
     marker.on('click', () => {
+      const isOpen = isCafeOpen(bookmark);
+      const statusClass = isOpen ? 'open' : 'closed';
+
+      const popupContent = `
+              <div class="popup-title">
+                  <img src="${instagramLogo}" alt="Instagram" width="20" height="20">
+                  <a href="${bookmark.instagramLink}" target="_blank" class="popup-title-text" style="font-size: 20px;">${bookmark.name}</a>
+              </div>
+              <div class="popup-status ${statusClass}">${isOpen ? 'Open' : 'Closed'}</div>
+              <div class="popup-address">${bookmark.address}</div>
+          `;
+
       document.getElementById('popup-content').innerHTML = popupContent;
       document.getElementById('custom-popup').style.display = 'block';
     });
